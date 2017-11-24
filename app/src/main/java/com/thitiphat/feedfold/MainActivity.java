9b9ff66f0e791d5c5867770f0d3848ec7bb890d7
Pattiiz <phat.my.name@gmail.com>
2017-11-25 00:59:27 +0700

    Create fragment

--- app/src/main/java/com/thitiphat/feedfold/MainActivity.java
+++ app/src/main/java/com/thitiphat/feedfold/MainActivity.java
@@ -5,7 +5,9 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.AsyncTask;
 import android.os.PersistableBundle;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.design.widget.NavigationView;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
@@ -33,7 +35,7 @@ import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
 
-public class MainActivity extends AppCompatActivity implements RecyclerViewClickListener {
+public class MainActivity extends AppCompatActivity implements RecyclerViewClickListener, NavigationView.OnNavigationItemSelectedListener {
 
     List<FeedModel> feedList = new ArrayList<>();
     String result, title, link, description, creator, ns = null;
@@ -61,6 +63,9 @@ public class MainActivity extends AppCompatActivity implements RecyclerViewClick
         drawerLayout = findViewById(R.id.drawer_layout);
         actionBarDrawerToggle = new ActionBarDrawerToggle(this, drawerLayout, R.string.open, R.string.close);
         drawerLayout.addDrawerListener(actionBarDrawerToggle);
+
+        NavigationView navigationView = findViewById(R.id.drawer_layout);
+        navigationView.setNavigationItemSelectedListener(this);
 
         SharedPreferences sharedPreferences = context.getSharedPreferences("list", Context.MODE_PRIVATE);
         SharedPreferences.Editor editor = sharedPreferences.edit();
@@ -129,6 +134,17 @@ public class MainActivity extends AppCompatActivity implements RecyclerViewClick
 //        intent.putExtra("ITEM", feedList.get(position).getTitle());
 //        startActivity(intent);
         return position;
+    }
+    
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        if (item.getItemId() == R.id.nav_timeline) {
+
+        }
+        if (item.getItemId() == R.id.nav_bookmark) {
+
+        }
+        return false;
     }
 
     public class LoadXml extends AsyncTask<List<String>, Void, Boolean> {
